9450850989044e6afb5cce21df7695c709e01ee6
Tammie Chung <tammie.chung11@gmail.com>
2019-09-16 17:13:07 +0000

    Created our own assertion function

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('🙌🙌🙌 Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('hello', 'hello');
+assertEqual(2, 3);
