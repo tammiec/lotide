cd7d2cc367fb4eec2da83c480bc9378193b6585c
Tammie Chung <tammie.chung11@gmail.com>
2019-09-17 19:50:53 +0000

    Create a function that can compare two arrays by iterating over each value and comparing each value

---
+++ eqArray.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙌🙌🙌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["hello", "goodbye"], ["hello", "goodbye"]), true);
