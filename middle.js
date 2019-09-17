d73f9a3c5c7419cc154bcf1c84c50cc306deedaa
Tammie Chung <tammie.chung11@gmail.com>
2019-09-17 20:40:01 +0000

    Edited with updated assertArraysEqual function

--- middle.js
+++ middle.js
@@ -9,16 +9,10 @@ const eqArrays = function(array1, array2) {
 };
 
 const assertArraysEqual = function(array1, array2) {
-  let assertion = true;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      assertion = false;
-    }
-  }
-  if (assertion === false) {
-    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${array1} !== ${array2}`);
-  } else {
+  if (eqArrays(array1, array2) === true) {
     console.log(`🙌🙌🙌 Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${array1} !== ${array2}`);
   }
 };
 
