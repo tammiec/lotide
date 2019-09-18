09342b08b0f5e097656258d8e96843a255fab743
Tammie Chung <tammie.chung11@gmail.com>
2019-09-18 17:11:56 +0000

    Create a function that counts only certain items in an array and outputs an object

---
+++ countOnly.js
@@ -0,0 +1,45 @@
+// ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙌🙌🙌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item]++;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+  "Agouhanna"
+];
+
+const result1 = countOnly(firstNames, { "Jason": false, "Karima": true, "Fang": true, 'Agouhanna': true});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], 2);
