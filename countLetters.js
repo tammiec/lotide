cac2ab743978a075e884dfcbb869f9167b05fbd5
Tammie Chung <tammie.chung11@gmail.com>
2019-09-18 17:26:13 +0000

    Count the letters in a string

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+// ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🙌🙌🙌 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// ACTUAL FUNCTION
+const countLetters = function(string) {
+  const results = {};
+  string = string.split(' ').join('');
+  for (const letter of string) {
+    if (results[letter]) {
+      results[letter]++;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+const ex1 = countLetters('lighthouse in the house');
+const ex2 = countLetters('hello how are you');
+
+assertEqual(ex1.h, 4);
+assertEqual(ex2.e, 2);
+assertEqual(ex1.m, undefined);
