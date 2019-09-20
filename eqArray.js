b484db1124c40135add5df4045e70542fe972616
Tammie Chung <tammie.chung11@gmail.com>
2019-09-20 21:32:33 +0000

    use recursion in eqArray.js

--- eqArray.js
+++ eqArray.js
@@ -10,9 +10,16 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
+
   for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else {
     if (array1[i] !== array2[i]) {
       return false;
+      }
     }
   }
   return true;
@@ -24,3 +31,7 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["hello", "goodbye"], ["hello", "goodbye"]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4, [6, 7]]], [[2, 3], [4, [6, 7]]]), true)
