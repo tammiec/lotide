b1f75e1526f1ec7aa5a7c11791efbdffaf8c6234
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 20:56:28 +0000

    implemented chai/mocha tests to eqArrays - acted as driver while @TYLER-JM navigated

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,23 @@
 const eqArrays = require('../eqArray');
 const assert = require('chai').assert;
 
+describe('#eqArrays', () => {
+  it('should return true for [1, 2, 3] & [1, 2, 3]', () => {
+    assert.equal(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it('should return false for ["1", "2", "3"] & ["1", "2", 3]', () => {
+    assert.equal(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it('should return true for [[2, 3], [4, [6, 7]]] & [[2, 3], [4, [6, 7]]]', () => {
+    assert.equal(eqArrays([[2, 3], [4, [6, 7]]], [[2, 3], [4, [6, 7]]]), true);
+  });
+  it('should return false for [[2, 3], [4, [6, 7]]] & [[2, 3], [4, [6, 7]]]', () => {
+    const nested1 = [1, [2, 3, 4], 5, 6, [7, [8, 9]]];
+    const nested1Twin = [1, [2, 3, 4], 5, "6", [7, [8, 9]]];
+    assert.equal(eqArrays(nested1, nested1Twin), false);
+  });
+});
+
 describe 
 
 // const assertEqual = require('../assertEqual');
