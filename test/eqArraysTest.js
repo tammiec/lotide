d0952bf586d8c09b5a8f43cf6a2c1fd693a4cea1
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 20:57:05 +0000

    implemented chai/mocha tests to eqArrays - acted as driver while @TYLER-JM navigated

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -17,19 +17,3 @@ describe('#eqArrays', () => {
     assert.equal(eqArrays(nested1, nested1Twin), false);
   });
 });
-
-describe 
-
-// const assertEqual = require('../assertEqual');
-// 
-
-// // TEST CODE
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays(["hello", "goodbye"], ["hello", "goodbye"]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-// assertEqual(eqArrays([[2, 3], [4, [6, 7]]], [[2, 3], [4, [6, 7]]]), true);
