0462c0aa1995d24d1a7c09cdfefdf0887764912d
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 22:30:21 +0000

    remove old test code

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -15,13 +15,3 @@ describe('#countLetters', () => {
     assert.doesNotHaveAnyKeys(countLetters('hello how are you'), [' ']);
   });
 });
-
-// const assertEqual = require('../assertEqual');
-// 
-// // TEST CODE
-// const ex1 = countLetters('lighthouse in the house');
-// const ex2 = countLetters('hello how are you');
-
-// assertEqual(ex1.h, 4);
-// assertEqual(ex2.e, 2);
-// assertEqual(ex1.m, undefined);
