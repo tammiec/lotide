f4afa9d3a334f05ef019e1fa4fc8995a734f0c4f
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 19:38:34 +0000

    converted tests to use chai/mocha for flatten

--- flatten.js
+++ flatten.js
@@ -1,15 +1,13 @@
 // ACTUAL FUNCTION
 const flatten = function(array) {
   let flat = [];
-  for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      for (let j = 0; j < array[i].length; j++) {
-        flat.push(array[i][j]);
-      }
-    } else {
-      flat.push(array[i]);
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      flat = flat.concat(flatten(item));
+      } else {
+      flat.push(item);
     }
-  }
+  });
   return flat;
 };
 
--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,8 +1,14 @@
-const assertArraysEqual = require('../assertArraysEqual');
 const flatten = require('../flatten');
-// TEST CASES
-console.log(flatten([1, 2, [3, 4], 5, [6]]));
-console.log(flatten(['apples', 'oranges', [3, 4, 5], 'bananas']));
-console.log(flatten([1, [2, 3, 4], 5]));
-assertArraysEqual(flatten([1, [2, 3, 4], 5]), [1, 2, 3, 4, 5]);
-assertArraysEqual(flatten([[1], 2, 3, [4, 5, 6]]), flatten([1, 2, [3, 4], 5, [6]]));
+const assert = require('chai').assert;
+
+describe('#flatten', () => {
+  it('should return [1, 2, 3, 4, 5] when input is [1, [2, 3, 4], 5]', () => {
+    assert.deepEqual(flatten([1, [2, 3, 4], 5]), [1, 2, 3, 4, 5]);
+  });
+  it('should return [1, 2, 3, 4, 5, 6] when input is [[1], 2, 3, [4, [5, 6]]]', () => {
+    assert.deepEqual(flatten([[1], 2, 3, [4, [5, 6]]]), [1, 2, 3, 4, 5, 6]);
+  });
+  it('should return array length of 10 when input is [[1], [2, 3, 4], [5, 6], [7, [8, 9]], 10]', () => {
+    assert.strictEqual(flatten([[1], [2, 3, 4], [5, 6], [7, [8, 9]], 10]).length, 10);
+  });
+});
