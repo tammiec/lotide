92b9d63e58894d0774c7c3cfa7c147ec180b5c28
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 16:10:08 +0000

    refactored tail function and created separate test file

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🙌🙌🙌 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🙅🏻‍♀️🙅🏻‍♀️🙅🏻‍♀️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let arrayTail = [];
   for (let i = 0; i < array.length; i++) {
@@ -16,21 +8,4 @@ const tail = function(array) {
   return arrayTail;
 };
 
-// Test Case 1: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case 2: working around array issue
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 3: An array with only one element should yield an empty array for its tail
-const empty = tail(["Hello"]);
-assertEqual(empty.length, 0);
-
-// Test Case 4: An empty array should yield an empty array for its tail
-const empty2 = tail([]);
-assertEqual(empty2.length, 0);
+module.exports = tail;
