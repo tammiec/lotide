28e9bd9642b63dedaa141abc5b9e631c1e27beb2
Tammie Chung <tammie.chung11@gmail.com>
2019-09-23 21:35:32 +0000

    implemented mocha/chai tests for countLetters test

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,9 +1,27 @@
-const assertEqual = require('../assertEqual');
 const countLetters = require('../countLetters');
-// TEST CODE
-const ex1 = countLetters('lighthouse in the house');
-const ex2 = countLetters('hello how are you');
+const assert = require('chai').assert;
 
-assertEqual(ex1.h, 4);
-assertEqual(ex2.e, 2);
-assertEqual(ex1.m, undefined);
+describe('#countLetters', () => {
+  it('should return 4 for letter h in lighthouse in the house', () => {
+    assert.strictEqual(countLetters('lighthouse in the house').h, 4);
+  });
+  it('should return undefined for letter m in hello', () => {
+    assert.isUndefined(countLetters('hello').m);
+  });
+  it("should return object for the function's return value", () => {
+    assert.isObject(countLetters('hello'));
+  });
+  it("should not count spaces", () => {
+    assert.doesNotHaveAnyKeys(countLetters('hello how are you'), [' ']);
+  });
+});
+
+// const assertEqual = require('../assertEqual');
+// 
+// // TEST CODE
+// const ex1 = countLetters('lighthouse in the house');
+// const ex2 = countLetters('hello how are you');
+
+// assertEqual(ex1.h, 4);
+// assertEqual(ex2.e, 2);
+// assertEqual(ex1.m, undefined);
